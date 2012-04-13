532eba60b0a8ff8f4298d333370c4e62f9866a7c
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2012-04-13 19:55:34 +0200

    add social links

--- config/compass.rb
+++ config/compass.rb
@@ -8,11 +8,13 @@ http_path             = '/'
 http_stylesheets_path = "/stylesheets"
 http_javascripts_path = "/stylesheets"
 http_images_path      = "/images"
+http_generated_images_path = "/images"
 
 css_dir         = 'public/stylesheets'
 sass_dir        = 'content/stylesheets'
 images_dir      = 'content/images'
 javascripts_dir = 'content/javascripts'
+generated_images_path = "content/images"
 
 output_style    = ENV['RACK_ENV'] != 'production' ? :nested : :compressed
 
